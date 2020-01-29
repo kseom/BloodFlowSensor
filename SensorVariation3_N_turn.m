bea804d703b4ba735b9bbdcdc6c5734302f1a860
SeongHoon-Jo <jayjo2005@naver.com>
2020-01-30 03:29:52 +0900

    수정하였습니다.
    SensorVariation1~2에서 MagneticField를 EMF Voltage로 수정하였고, Coil의 값(Turn수, Coil의 radius)들을 바꾸어가며 실험 했습니다.

--- SensorVariation3_N_turn.m
+++ SensorVariation3_N_turn.m
@@ -1,4 +1,4 @@
-%% Sensor Variation1 (Position)
+%% Sensor Variation3 (N_turn size of the coil)
 % Peak EMF voltage to Distance between coil and RBC (Position)
 %% Magnetic Field Setting
 
@@ -10,26 +10,24 @@ d_int = 1e-4; % interparticle spacing
 
 N = 1; % Number of positive point charge consisting a group / (2.464e-12 / 1.6e-19)
 v = 1; % meter per second
-dura = 0.005;
+dura = 0.01;
 interval = 1e-6;
 
 graphType = 'x';
-x_pnt = 0;
-% x_pnt = 0; y_pnt = 0.5e-4; % location of the sensor
+x_pnt = 0; y_pnt = 0.5e-4; % location of the sensor
+
 %% Coil settings
 
-N_turn = 50;    % 임의의 값
+%N_turn = 50;    % 임의의 값
 r = 5e-3;      % Radius of coil [m]
 d = 5e-2;     % Length of coil [m]
 
-%% Position of Coil(Sensor)
+%% Varify the Number of Turns of Coil(sensor)
 
-pos=5;
-PeakVal = [];
-for i=1:pos;
-    y_pnt = (i+2) * 1e-5;
-%    [X_data, Y_data] = AddMagneticField(N_p,d_int,graphType,x_pnt,y_pnt,interval,N,v);
-%    [pks,locs] = findpeaks(Y_data);
+Turn_No_max = 5;
+PeakTime = [];
+for i=1:Turn_No_max;
+    N_turn = (i) * 50; % # of Turns
     [time,MField] = RateChangeOfMagneticField(N_p,d_int,graphType,x_pnt,y_pnt,dura,interval,N,v);
     timeVaryingMField = (MField(2:end)-MField(1:end-1))./(time(2)-time(1));
     emf = -N_turn * timeVaryingMField * pi * (r^2);
@@ -38,8 +36,8 @@ end
 
 %% Plot (Distance between Coil and RBCs - PeakVal) Graph
 
-DistSensor = linspace(3e-5, (pos+2)*1e-5, pos);
+Turns = linspace(50, Turn_No_max*50, Turn_No_max);
 figure;
-plot(DistSensor,PeakVal);
-title('Sensor Variation1 - Position');
-xlabel('Distance between Coil and RBCs'); ylabel('Peak EMF Voltage');
+plot(Turns,PeakVal);
+title('Sensor Variation3 - N_Turn');
+xlabel('Number of Turns of Coil'); ylabel('Peak EMF Voltage');
